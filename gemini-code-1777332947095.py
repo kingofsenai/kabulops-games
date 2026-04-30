e4742a92bdf6ee0795eb589d66bff23c2296cfd5
kingofsenai <jr.esportivo@gmail.com>
2026-04-29 23:07:40 -0300

    Update gemini-code-1777332947095.py

--- gemini-code-1777332947095.py
+++ gemini-code-1777332947095.py
@@ -39,86 +39,15 @@ def carregar_biblioteca_estatica():
     return [
         # --- BIBLIOTECA DE PERSONAGENS (COMPLETA - 62 REGISTROS) ---
 @st.cache_data
+@st.cache_data
 def carregar_biblioteca_estatica():
     return [
-        # --- JOGOS DA ENQUETE (2 POR JOGO) ---
         {"nome": "Suicune", "jogo": "Pokémon Crystal (GBC)", "papel": "Lendário", "golpe": "Aurora Beam / Hydro Pump"},
         {"nome": "Eusine", "jogo": "Pokémon Crystal (GBC)", "papel": "Treinador", "golpe": "Dream Eater / Mean Look"},
-        {"nome": "Alis Landale", "jogo": "Phantasy Star (Master)", "papel": "Protagonista", "golpe": "Fire Spell / Light Sword"},
-        {"nome": "Myau", "jogo": "Phantasy Star (Master)", "papel": "Guardião", "golpe": "Cure Magic / Trap Disarm"},
-        {"nome": "Chrono", "jogo": "Chrono Trigger (SNES)", "papel": "Herói", "golpe": "Luminaire / Cyclone"},
-        {"nome": "Frog", "jogo": "Chrono Trigger (SNES)", "papel": "Cavaleiro", "golpe": "Water 2 / Leap Slash"},
-        {"nome": "Sonic", "jogo": "Sonic CD (Sega CD)", "papel": "Velocidade", "golpe": "Spin Dash / Super Peel Out"},
-        {"nome": "Metal Sonic", "jogo": "Sonic CD (Sega CD)", "papel": "Antagonista", "golpe": "Maximum Overdrive / V. Shield"},
-        {"nome": "Nights", "jogo": "Nights into Dreams (Saturn)", "papel": "Espírito", "golpe": "Paraloop / Drill Dash"},
-        {"nome": "Reala", "jogo": "Nights into Dreams (Saturn)", "papel": "Rival", "golpe": "Nightmaren Slash / Illusion"},
-        {"nome": "Solid Snake", "jogo": "Metal Gear Solid (PS1)", "papel": "Espião", "golpe": "CQC / Stinger Missile"},
-        {"nome": "Gray Fox", "jogo": "Metal Gear Solid (PS1)", "papel": "Ciborgue Ninja", "golpe": "Stealth Camouflage / Katana Slash"},
-        {"nome": "James Bond", "jogo": "GoldenEye 007 (N64)", "papel": "Agente", "golpe": "Remote Mines / Golden Gun"},
-        {"nome": "Alec Trevelyan", "jogo": "GoldenEye 007 (N64)", "papel": "Vilão", "golpe": "Dual RCP-90 / Tactical Strike"},
-        {"nome": "Axel Stone", "jogo": "Streets of Rage 2 (Mega Drive)", "papel": "Lutador", "golpe": "Grand Upper / Dragon Wing"},
-        {"nome": "Blaze Fielding", "jogo": "Streets of Rage 2 (Mega Drive)", "papel": "Lutadora", "golpe": "Kikousho / Somersault Kick"},
-
-        # --- NOVOS: STREET FIGHTER II ---
-        {"nome": "Ryu", "jogo": "Street Fighter II", "papel": "Lutador", "golpe": "Hadouken / Shoryuken"},
-        {"nome": "Chun-Li", "jogo": "Street Fighter II", "papel": "Interpol", "golpe": "Hyakuretsu Kyaku / Spinning Bird Kick"},
-        {"nome": "Guile", "jogo": "Street Fighter II", "papel": "Major", "golpe": "Sonic Boom / Flash Kick"},
-        {"nome": "M. Bison", "jogo": "Street Fighter II", "papel": "Vilão", "golpe": "Psycho Crusher / Scissor Kick"},
-
-        # --- NOVOS: MORTAL KOMBAT II ---
-        {"nome": "Liu Kang", "jogo": "Mortal Kombat II", "papel": "Campeão", "golpe": "Dragon Fireball / Bicycle Kick"},
-        {"nome": "Scorpion", "jogo": "Mortal Kombat II", "papel": "Espectro", "golpe": "Spear / Teleport Punch"},
-        {"nome": "Sub-Zero", "jogo": "Mortal Kombat II", "papel": "Ninja", "golpe": "Ice Blast / Slide Attack"},
-        {"nome": "Kitana", "jogo": "Mortal Kombat II", "papel": "Princesa", "golpe": "Fan Toss / Square Wave Punch"},
-
-        # --- NOVOS: CADILLACS AND DINOSAURS ---
-        {"nome": "Jack Tenrec", "jogo": "Cadillacs and Dinosaurs", "papel": "Mecânico", "golpe": "Dashing Uppercut / Slide Kick"},
-        {"nome": "Hannah Dundee", "jogo": "Cadillacs and Dinosaurs", "papel": "Diplomata", "golpe": "Spinning Kick / Knife Mastery"},
-        {"nome": "Mustapha Cairo", "jogo": "Cadillacs and Dinosaurs", "papel": "Engenheiro", "golpe": "Flying Kick / Tornado Kick"},
-        {"nome": "Mess O'Bradovich", "jogo": "Cadillacs and Dinosaurs", "papel": "Gigante", "golpe": "Power Slam / Flying Body Press"},
-
-        # --- POKÉMON (1ª GERAÇÃO) ---
-        {"nome": "Kabutops", "jogo": "Pokémon Yellow/Red/Blue", "papel": "Fóssil", "golpe": "Slash / Hydro Pump"},
-        {"nome": "Pikachu", "jogo": "Pokémon Yellow", "papel": "Mascote", "golpe": "Thunderbolt / Quick Attack"},
-        {"nome": "Charizard", "jogo": "Pokémon Red/Blue", "papel": "Fogo/Voador", "golpe": "Flamethrower / Fire Blast"},
-        {"nome": "Blastoise", "jogo": "Pokémon Red/Blue", "papel": "Água", "golpe": "Hydro Pump / Skull Bash"},
-        {"nome": "Venusaur", "jogo": "Pokémon Red/Blue", "papel": "Planta/Veneno", "golpe": "Solar Beam / Sleep Powder"},
-        {"nome": "Mewtwo", "jogo": "Pokémon Red/Blue", "papel": "Psíquico", "golpe": "Psychic / Recover"},
-        {"nome": "Gengar", "jogo": "Pokémon Red/Blue", "papel": "Fantasma", "golpe": "Shadow Ball / Confuse Ray"},
-        {"nome": "Dragonite", "jogo": "Pokémon Red/Blue", "papel": "Dragão", "golpe": "Hyper Beam / Dragon Rage"},
-        {"nome": "Snorlax", "jogo": "Pokémon Red/Blue", "papel": "Tanque", "golpe": "Body Slam / Rest"},
-        {"nome": "Gyarados", "jogo": "Pokémon Red/Blue", "papel": "Água/Dragão", "golpe": "Dragon Dance / Hydro Pump"},
-        {"nome": "Arcanine", "jogo": "Pokémon Red/Blue", "papel": "Fogo", "golpe": "Extreme Speed / Flare Blitz"},
-        {"nome": "Alakazam", "jogo": "Pokémon Red/Blue", "papel": "Psíquico", "golpe": "Teleport / Psybeam"},
-
-        # --- CLÁSSICOS NINTENDO (64/SNES) ---
-        {"nome": "Mario", "jogo": "Super Mario 64", "papel": "Herói", "golpe": "Triple Jump / Ground Pound"},
-        {"nome": "Link", "jogo": "Ocarina of Time", "papel": "Herói", "golpe": "Spin Attack / Din's Fire"},
-        {"nome": "Fox McCloud", "jogo": "Star Fox 64", "papel": "Líder", "golpe": "Barrel Roll / Nova Bomb"},
-        {"nome": "Donkey Kong", "jogo": "Donkey Kong 64", "papel": "Líder", "golpe": "Giant Punch / Coconut Cannon"},
-        {"nome": "Samus Aran", "jogo": "Super Metroid", "papel": "Caçadora", "golpe": "Screw Attack / Ice Beam"},
-        {"nome": "Yoshi", "jogo": "Yoshi's Island", "papel": "Aliado", "golpe": "Egg Throw / Flutter Jump"},
-        {"nome": "Kirby", "jogo": "Kirby Super Star", "papel": "Herói", "golpe": "Copy Ability / Star Spit"},
-        {"nome": "Captain Falcon", "jogo": "F-Zero X", "papel": "Piloto", "golpe": "Falcon Punch / Falcon Kick"},
-
-        # --- CLÁSSICOS PLAYSTATION 1 ---
-        {"nome": "Cloud Strife", "jogo": "Final Fantasy VII", "papel": "Mercenário", "golpe": "Omnislash / Cross-Slash"},
-        {"nome": "Sephiroth", "jogo": "Final Fantasy VII", "papel": "Antagonista", "golpe": "Supernova / Octaslash"},
-        {"nome": "Leon Kennedy", "jogo": "Resident Evil 2", "papel": "Policial", "golpe": "Shotgun Blast / Knife Strike"},
-        {"nome": "Crash Bandicoot", "jogo": "Crash Bandicoot", "papel": "Mascote", "golpe": "Spin Attack / Body Slam"},
-        {"nome": "Spyro", "jogo": "Spyro the Dragon", "papel": "Dragão", "golpe": "Flame Breath / Charge"},
-        {"nome": "Jin Kazama", "jogo": "Tekken 3", "papel": "Lutador", "golpe": "Laser Scraper / Electric Wind"},
-        {"nome": "Lara Croft", "jogo": "Tomb Raider", "papel": "Arqueóloga", "golpe": "Dual Pistols / Handstand"},
-
-        # --- RETRÔ ARCADE / MASTER / MEGA ---
-        {"nome": "Alex Kidd", "jogo": "Alex Kidd in Miracle World", "papel": "Príncipe", "golpe": "Janken Punch / Peticopter"},
-        {"nome": "Mega Man X", "jogo": "Mega Man X", "papel": "Hunter", "golpe": "X-Buster / Dash Attack"},
-        {"nome": "Zero", "jogo": "Mega Man X", "papel": "Hunter", "golpe": "Z-Saber / Ryuenjin"},
-        {"nome": "Terry Bogard", "jogo": "Fatal Fury", "papel": "Lutador", "golpe": "Power Wave / Burn Knuckle"},
-        {"nome": "Mai Shiranui", "jogo": "King of Fighters", "papel": "Kunoichi", "golpe": "Kacho Sen / Ryuubi no Mai"},
+        # ... (todos os outros personagens ficam aqui no meio) ...
         {"nome": "Kyo Kusanagi", "jogo": "King of Fighters", "papel": "Lutador", "golpe": "Orochinagi / 100 Shiki"},
         {"nome": "Shinobi", "jogo": "The Revenge of Shinobi", "papel": "Ninja", "golpe": "Shuriken Throw / Mijin Jutsu"}
-    ]
+    ] # <--- ESTE COLCHETE É O QUE ESTÁ FALTANDO!
 
 # --- 4. BARRA LATERAL (Com as 5 Pokébolas) ---
 with st.sidebar:
